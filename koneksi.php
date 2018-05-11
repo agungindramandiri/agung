6dfef2a54806a503b5bbd7ff882d5dbc5f5bd233
agungindramandiri <39194692+agungindramandiri@users.noreply.github.com>
2018-05-12 01:19:25 +1000

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,4 @@
+<?php
+	$connect = mysqli_connect('localhost','root','','tubes') or die('Server is down!');
+
+?>
---
+++ panggil.php
@@ -0,0 +1,7 @@
+<?php
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+$error = true;
+$goto = "..\home.php";
+
+include 'account_'
